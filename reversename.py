034858527a0c32c2df1ccfc4ef2cf8d678f8b475
sanjaysir9191 <132204418+sanjaysir9191@users.noreply.github.com>
2024-02-15 18:58:25 +0530

    Add files via upload

---
+++ reversename.py
@@ -0,0 +1,16 @@
+first_name=input("enter your first name=")
+second_name=input("enter your second name=")
+print(first_name +" "+ second_name)
+print(second_name +" "+ first_name)
+
+print(second_name)
+ 
+# change into list & tuole
+x=1,2,3,4,5,6
+print(list(x))
+print(tuple(x))
+
+a=input("enter you file name & format=")
+s=a.split(".")
+
+print(s[-1])
